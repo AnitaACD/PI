6483ccc277f0688da5aeae406d4daccc630519b6
ineshv999 <123046059@upq.edu.mx>
2026-04-14 01:53:10 -0600

    Change login route test to home route

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,5 +9,5 @@ def test_login_route():
     sys.path.insert(0, 'SistemaGestion_2')
     from app import app
     client = app.test_client()
-    response = client.get('/login')
+    response = client.get('/')
     assert response.status_code == 200
