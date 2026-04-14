6dfbd75665f9eb8ac8b607f49f96336e4bc2fda3
ineshv999 <123046059@upq.edu.mx>
2026-04-14 01:34:02 -0600

    Refactor database connection to use environment variables

--- SistemaGestion_2/db.py
+++ SistemaGestion_2/db.py
@@ -1,12 +1,13 @@
 # db.py
+import os
 import pyodbc
 
-server = "localhost,14333"  # ej: localhost\SQLEXPRESS
-database = "GestionActivos"
-username = "sa"
-password = "SqlServer@123"
+server = os.getenv('DB_HOST', 'localhost') + ',' + os.getenv('DB_PORT', '14334')
+database = os.getenv('DB_NAME', 'GestionActivos')
+username = os.getenv('DB_USER', 'sa')
+password = os.getenv('DB_PASSWORD', 'SqlServer@123')
 driver = "ODBC Driver 17 for SQL Server"
-
 conn_str = f"DRIVER={{{driver}}};SERVER={server};DATABASE={database};UID={username};PWD={password}"
+
 conn = pyodbc.connect(conn_str, autocommit=True)
 cursor = conn.cursor()
