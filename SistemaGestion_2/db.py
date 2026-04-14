1f6283848eab83daabecfbdd138308c7f44ed863
ineshv999 <123046059@upq.edu.mx>
2026-04-14 01:29:48 -0600

    Update db.py

--- SistemaGestion_2/db.py
+++ SistemaGestion_2/db.py
@@ -1,21 +1,12 @@
-import os
+# db.py
 import pyodbc
 
-# Leer variables de entorno (con valores por defecto por si acaso)
-server = os.getenv('DB_HOST', 'localhost') + ',' + os.getenv('DB_PORT', '14334')
-database = os.getenv('DB_NAME', 'GestionActivos')
-username = os.getenv('DB_USER', 'sa')
-password = os.getenv('DB_PASSWORD', 'SqlServer@123')
+server = "localhost,14333"  # ej: localhost\SQLEXPRESS
+database = "GestionActivos"
+username = "sa"
+password = "SqlServer@123"
 driver = "ODBC Driver 17 for SQL Server"
 
 conn_str = f"DRIVER={{{driver}}};SERVER={server};DATABASE={database};UID={username};PWD={password}"
-
-try:
-    conn = pyodbc.connect(conn_str, autoconnect=True, timeout=30)
-    print("Conexión exitosa a SQL Server")
-except Exception as e:
-    print(f"Error de conexión: {e}")
-    conn = None
-
-if conn:
-    cursor = conn.cursor()
+conn = pyodbc.connect(conn_str, autocommit=True)
+cursor = conn.cursor()
