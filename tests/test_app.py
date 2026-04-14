e05d9f089032ec3fc4fadfddee6a19c06d76cf91
ineshv999 <123046059@upq.edu.mx>
2026-04-14 00:21:07 -0600

    Create test_app.py

---
+++ tests/test_app.py
@@ -0,0 +1,13 @@
+def test_import_app():
+    import sys
+    sys.path.insert(0, 'SistemaGestion_2')
+    from app import app
+    assert app is not None
+
+def test_login_route():
+    import sys
+    sys.path.insert(0, 'SistemaGestion_2')
+    from app import app
+    client = app.test_client()
+    response = client.get('/login')
+    assert response.status_code == 200
